220a6d507deee970b0e16346ea332e349f1d91d9
jryusuf <yusufyuksel@aol.com>
2025-03-16 15:55:14 +0000

    unittest setup

--- test_functional.py
+++ test_functional.py
@@ -1,7 +1,24 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def test_can_start_a_todo_list(self):
+        #Edith has heard about a cool new online to-do app
+        #she goes to check out its homepage
+        self.browser.get("http://localhost:8000")
+
+        #she notices the page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+
+        #she is invited to entera to-do item straight away
+        self.fail("Finish the test")
+
+
+if __name__ == "__main__":
+    unittest.main()
