f5b0333ddd22ce550e2af151f4b4873e37d83bc7
jryusuf <yusufyuksel@aol.com>
2025-03-17 11:09:04 +0000

    add style centering

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -105,5 +105,29 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn("buy peacock feathers", page_text)
         self.assertIn("buy milk", page_text)
 
+    def test_layout_and_styling(self):
+        #edith goes to the home page
+        self.browser.get(self.live_server_url)
+
+        #her browser window is set to a very specific size
+        self.browser.set_window_size(1024, 768)
+
+        #she notices the input box is nicely centered
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"]+ inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
+
+        inputbox.send_keys("testing")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_now_in_list_table("1: testing")
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"]+ inputbox.size["width"] / 2,
+            512,
+            delta=10,
+        )
 if __name__ == "__main__":  
     unittest.main() 
