b8bf705789b9f67777274d0149a5861d3f162a74
jryusuf <yusufyuksel@aol.com>
2025-03-18 10:23:03 +0000

    enforce model validation in list view

--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -4,19 +4,38 @@ from selenium.webdriver.common.keys import Keys
 from unittest import skip
 
 class ItemValidationTest(FunctionalTest):
-    @skip
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-
+        self.wait_for(
+            lambda: self.assertEqual(
+                        self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                        "You can't have an empty list item"
+                    )    
+        )
         # She tries again with some text for the item, which now works
-
+        self.browser.find_element(By.ID, "id_new_item").send_keys("purchase  milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_now_in_list_table("1: purchase milk")
+        
+        
         # Perversely, she now decides to submit a second blank list item
-
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        
         # She receives a similar warning on the list page
-
+        self.wait_for(
+            lambda: self.assertEqual(
+                        self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                        "You can't have an empty list item"
+                    )    
+        )
         # And she can correct it by filling some text in
-        self.fail("write me!")
+        self.browser.find_element(By.ID, "id_new_item").send_keys("make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_now_in_list_table("2: make tea")
+        
--- src/lists/views.py
+++ src/lists/views.py
@@ -13,7 +13,18 @@ def home_page(request):
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
-    return render(request, "list.html", {"list": our_list})
+    error = None
+
+    if request.method == "POST":
+        try:
+            item = Item(text=request.POST["item_text"], list=our_list)  
+            item.full_clean()  
+            item.save()  
+            return redirect(f"/lists/{our_list.id}/")
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, "list.html", {"list": our_list, "error": error})
 
 def new_list(request):
     nulist = List.objects.create()
@@ -26,8 +37,3 @@ def new_list(request):
         error = escape("You can't have an empty list item")
         return render(request, "home.html", {"error": error})
     return redirect(f"/lists/{nulist.id}/")
-
-def add_item(request, list_id):
-    our_list = List.objects.get(id= list_id)
-    Item.objects.create(text= request.POST["item_text"], list= our_list)
-    return redirect(f"/lists/{our_list.id}/")
