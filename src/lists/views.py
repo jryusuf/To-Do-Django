eacd6d5f21039ecf283316bcd1f3577c68cadf16
jryusuf <yusufyuksel@aol.com>
2025-03-18 11:57:31 +0000

    use new form in home_page, functional tests breaks

--- src/lists/views.py
+++ src/lists/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import redirect,render
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 from django.utils.html import escape
+from lists.forms import ItemForm
 
 def home_page(request):
     # if request.method == "POST":
     #     Item.objects.create(text = request.POST["item_text"])
     #     return redirect("/lists/the-only-list-in-the-world/")
 
-    return render(request, "home.html")
+    return render(request, "home.html", {"form": ItemForm()})
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
